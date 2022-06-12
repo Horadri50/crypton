38c7201652ab686daced6473d5ad5c9a243963b1
horadrik50 <horadrik50@gmail.com>
2022-06-12 11:50:33 +0300

    add main class CodeCaesar

---
+++ src/CodeCaesar.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CodeCaesar {
+    private static final List<Character> alphabet = Arrays.asList('А', 'Б', 'В', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+            'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+            'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '0', '1', '2',
+            '3', '4', '5', '6', '7', '8', '9', '!', '?', '-', ';',':', ',', '.', ' ', '"');
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String scanner() {
+        return new Scanner(System.in).nextLine();
+    }
+}
